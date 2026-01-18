2bfd05b12bcc835255ed0c0e413ded202538c651
Bamtoliya <31297221+Bamtoliya@users.noreply.github.com>
2026-01-19 06:35:21 +0900

    PrototypeManager 추가

---
+++ Engine/Source/System/Manager/Private/PrototypeManager.cpp
@@ -0,0 +1,60 @@
+﻿#pragma once
+#include "PrototypeManager.h"
+#include "GameObject.h"
+#include "Component.h"
+IMPLEMENT_SINGLETON(PrototypeManager)
+
+#pragma region Constructor&Destructor
+EResult PrototypeManager::Initialize(void* arg)
+{
+	return EResult::Success;
+}
+
+void PrototypeManager::Free()
+{
+	for (auto& pair : m_GameObjectPrototypeMap)
+		Safe_Release(pair.second);
+	m_GameObjectPrototypeMap.clear();
+	for (auto& pair : m_ComponentPrototypeMap)
+		Safe_Release(pair.second);
+	m_ComponentPrototypeMap.clear();
+}
+#pragma endregion
+
+#pragma region Prototype Management
+EResult PrototypeManager::RegisterGameObjectPrototype(const wstring& prototypeTag, GameObject* prototype)
+{
+	if(!prototype || m_GameObjectPrototypeMap.find(prototypeTag) != m_GameObjectPrototypeMap.end())
+		return EResult::InvalidArgument;
+
+	m_GameObjectPrototypeMap.emplace(prototypeTag, prototype);
+	//Safe_AddRef(prototype);
+	return EResult::Success;
+}
+
+GameObject* PrototypeManager::CreateGameObjectPrototype(const wstring& prototypeTag, void* arg)
+{
+	auto	iter = m_GameObjectPrototypeMap.find(prototypeTag);
+	if (iter == m_GameObjectPrototypeMap.end())
+		return nullptr;
+	return iter->second->Clone(arg);
+}
+EResult PrototypeManager::RegisterComponentPrototype(const wstring& prototypeTag, Component* prototype)
+{
+	if (!prototype || m_ComponentPrototypeMap.find(prototypeTag) != m_ComponentPrototypeMap.end())
+		return EResult::InvalidArgument;
+	m_ComponentPrototypeMap.emplace(prototypeTag, prototype);
+	//Safe_AddRef(prototype);
+	return EResult::Success;
+}
+
+Component* PrototypeManager::CreateComponentPrototype(const wstring& prototypeTag, GameObject* owner, void* arg)
+{
+	auto	iter = m_ComponentPrototypeMap.find(prototypeTag);
+	if (iter == m_ComponentPrototypeMap.end())
+		return nullptr;
+	return iter->second->Clone(owner, arg);
+}
+
+#pragma endregion
+
---
+++ Engine/Source/System/Manager/Public/PrototypeManager.h
@@ -0,0 +1,32 @@
+﻿#pragma once
+
+
+#include "Base.h"
+
+BEGIN(Engine)
+class ENGINE_API PrototypeManager : public Base
+{
+	DECLARE_SINGLETON(PrototypeManager)
+#pragma region Constructor&Destructor
+private:
+	PrototypeManager() {}
+	virtual ~PrototypeManager() = default;
+	EResult Initialize(void* arg = nullptr);
+public:
+	virtual void Free() override;
+#pragma endregion
+#pragma region Prototype Management
+public:
+	EResult RegisterGameObjectPrototype(const wstring& prototypeTag, class GameObject* prototype);
+	class GameObject* CreateGameObjectPrototype(const wstring& prototypeTag, void* arg = nullptr);
+public:
+	EResult RegisterComponentPrototype(const wstring& prototypeTag, class Component* prototype);
+	class Component* CreateComponentPrototype(const wstring& prototypeTag, class GameObject* owner, void* arg = nullptr);
+#pragma endregion
+#pragma region Variable
+private:
+	unordered_map<wstring, class Component*> m_ComponentPrototypeMap;
+	unordered_map<wstring, class GameObject*> m_GameObjectPrototypeMap;
+#pragma endregion
+};
+END
