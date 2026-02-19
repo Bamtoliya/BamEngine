f163a87ae6b980aa8c08f3a466c306942ff1eea2
Bamtoliya <31297221+Bamtoliya@users.noreply.github.com>
2026-02-20 03:15:36 +0900

    box2dCollider 추가

---
+++ Engine/Source/Physics/Components/Collider/Public/Box2DCollider.h
@@ -0,0 +1,27 @@
+﻿#pragma once
+
+#include "Collider.h"
+
+BEGIN(Engine)
+class ENGINE_API Box2DCollider final : public Collider
+{
+	REFLECT_CLASS(Box2DCollider, Collider)
+#pragma region Constructor&Destructor
+private:
+	Box2DCollider();
+	virtual ~Box2DCollider() = default;
+	EResult Initialize(void* arg = nullptr) override;
+public:
+	static Box2DCollider* Create(void* arg = nullptr);
+	virtual Component* Clone(GameObject* owner, void* arg = nullptr) override;
+	virtual void Free() override;
+#pragma endregion
+
+
+#pragma region Member Variable
+private:
+	Rect m_Rect;
+#pragma endregion
+
+}
+END
--- Engine/Source/Physics/Public/Collision.h
+++ Engine/Source/Physics/Public/Collision.h
@@ -10,27 +10,25 @@ public:
 	static bool Intersects(const Ray& ray, const AABB& aabb, HitResult& outHitResult);
 	static bool Intersects(const Ray& ray, const AABB& aabb, const mat4& worldMatrix, HitResult& outHitResult);
 	static bool Intersects(const Ray& ray, const BoundingSphere& sphere, HitResult& outHitResult);
-
-	
+public:
 	static bool Contains(const AABB& aabb, const vec3& point);
 	static bool Contains(const AABB& aabb, const mat4& worldMatrix, const vec3& point);
 	static bool Contains(const BoundingSphere& sphere, const vec3& point);
-
+public:
 	static bool Intersects(const AABB& a, const AABB& b);
 	static bool Intersects(const AABB&a, const AABB& b, HitResult& outHitResult);
-	
+public:
 	static bool Intersects(const BoundingSphere& a, const BoundingSphere& b);
 	static bool Intersects(const BoundingSphere& a, const BoundingSphere& b, HitResult& outHitResult);
-
+public:
 	static bool Intersects(const AABB& aabb, const BoundingSphere& sphere);
 	static bool Intersects(const AABB& aabb, const BoundingSphere& sphere, HitResult& outHitResult);
-
+public:
 	static bool Intersects(const AABB& a, const AABB& b, const mat4& worldMatrixb);
 	static bool Intersects(const AABB& a, const AABB& b, const mat4& worldMatrixb, HitResult& outHitResult);
-
+public:
 	static bool Intersects(const AABB& a, const mat4& worldMatrixa, const AABB& b, const mat4& worldMatrixb);
 	static bool Intersects(const AABB& a, const mat4& worldMatrixa, const AABB& b, const mat4& worldMatrixb, HitResult& outHitResult);
-
 public:
 	static bool Sweep(const AABB& box, const vec3& direction, f32 distance, HitResult& outHitResult);
 	static bool Sweep(const BoundingSphere& sphere, const vec3& direction, f32 distance, HitResult& outHitResult);
